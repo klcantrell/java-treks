2386996307be6e066e82f093417bce46f0324496
klcantrell <cantrellkalalau@gmail.com>
2019-01-07 21:38:39 -0500

    variables and int

--- bulchalka-master/Hello_World/src/Hello.java
+++ bulchalka-master/Hello_World/src/Hello.java
@@ -1,8 +1,16 @@
 public class Hello {
 
     public static void main(String[] args) {
-
         System.out.println("Hello, KALALAU!");
 
+        int myFirstNumber = 5;
+        int mySecondNumber = 10;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
     }
 }
