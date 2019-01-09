aeaced4f55dd0fdac38546a9be42b0b67fd2d16b
klcantrell <cantrellkalalau@gmail.com>
2019-01-08 21:19:08 -0500

    char and boolean

---
+++ bulchalka-master/CharAndBoolean/src/com/kalcantrell/Main.java
@@ -0,0 +1,12 @@
+package com.kalcantrell;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // width of 16 (2 bytes)
+        char myChar = '\u02E7';
+        System.out.println(myChar);
+
+        boolean myBoolean = false;
+    }
+}
