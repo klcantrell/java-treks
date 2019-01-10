17fbead5396a0b21430564fba2464c6f91794dbc
klcantrell <cantrellkalalau@gmail.com>
2019-01-10 07:59:19 -0500

    methods WIP

---
+++ bulchalka-master/Methods/src/com/kalcantrell/Main.java
@@ -0,0 +1,31 @@
+package com.kalcantrell;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            return finalScore;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + ", your position on the high score table is " + position);
+    }
+
+    public static int calculateHighScorePosition()
+}
