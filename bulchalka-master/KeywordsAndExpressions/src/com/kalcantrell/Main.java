b034763e303003e618487680075e65cab1ab86dd
klcantrell <cantrellkalalau@gmail.com>
2019-01-09 21:53:23 -0500

    basic expressions

---
+++ bulchalka-master/KeywordsAndExpressions/src/com/kalcantrell/Main.java
@@ -0,0 +1,19 @@
+package com.kalcantrell;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double kilometers = (100 * 1.609344);
+	    int highScore = 50;
+	    if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+
+	    int score = 100; // expression is score = 100
+        if (score > 90) { // expression is score > 90
+            System.out.println("you got the high score!"); // expression is "you got the high score!"
+            score = 0; // expression is score = 0
+        }
+    }
+}
