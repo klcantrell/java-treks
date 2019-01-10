85340032b55832a7ce0dc7c0055f285f48b665c8
klcantrell <cantrellkalalau@gmail.com>
2019-01-09 22:01:32 -0500

    statements, whitespace, and indenting

---
+++ bulchalka-master/StatementsWhitespaceAndIndenting/src/com/kalcantrell/Main.java
@@ -0,0 +1,14 @@
+package com.kalcantrell;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myValue = 50; // statement is entire line
+        myValue++; // statement
+        System.out.println("A string"); // this whole line is a statement
+        System.out.println("This is a " +
+                "longer " +
+                "string"); // entire thing is a statement
+
+    }
+}
