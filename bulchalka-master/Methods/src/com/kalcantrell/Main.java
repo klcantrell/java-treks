fef02f2c0d9efacba677159f188635134d7237c2
klcantrell <cantrellkalalau@gmail.com>
2019-01-10 22:06:59 -0500

    basic methods

--- bulchalka-master/Methods/src/com/kalcantrell/Main.java
+++ bulchalka-master/Methods/src/com/kalcantrell/Main.java
@@ -4,12 +4,20 @@ package com.kalcantrell;
 public class Main {
 
     public static void main(String[] args) {
+        int highScore;
 
-        int highScore = calculateScore(true, 800, 5, 100);
-        System.out.println("Your final score was " + highScore);
+//        highScore = calculateScore(true, 800, 5, 100);
 
-        highScore = calculateScore(true, 10000, 8, 200);
-        System.out.println("Your final score was " + highScore);
+
+        displayHighScorePosition("Kal", calculateHighScorePosition(1500));
+
+        displayHighScorePosition("Cloud", calculateHighScorePosition(900));
+
+        displayHighScorePosition("Squall", calculateHighScorePosition(400));
+
+        displayHighScorePosition("Noctis", calculateHighScorePosition(50));
+
+
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -27,5 +35,17 @@ public class Main {
         System.out.println(playerName + ", your position on the high score table is " + position);
     }
 
-    public static int calculateHighScorePosition()
+    public static int calculateHighScorePosition(int score) {
+        int position;
+        if (score >= 1000) {
+            position = 1;
+        } else if (score >= 500) {
+            position = 2;
+        } else if (score >= 100) {
+            position = 3;
+        } else {
+            position = 4;
+        }
+        return position;
+    }
 }
