32aa37f7208b599155f83325ae9d9a0782339354
klcantrell <cantrellkalalau@gmail.com>
2019-01-17 22:20:16 -0500

    buchalka: WIP intro to arraylists

---
+++ bulchalka-master/ListAndArrayList/src/com/app/GroceryList.java
@@ -0,0 +1,28 @@
+package com.app;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You've currently got " + groceryList.size() + " item in your grocery list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem (int position, String newItem) {
+        groceryList.set(position, newItem)
+        System.out.println("Grocery item " + position + " has been modified");
+    }
+
+    public void removeGroceryItem(int position) {
+        String theItems = groceryList.get(position);
+        groceryList.remove(position);
+    }
+}
