ce8b782be5a05d2e06f746fa662450015efbe5c1
klcantrell <cantrellkalalau@gmail.com>
2019-01-27 16:21:56 -0500

    buchalka: collections

--- buchalka-master/Collections/src/com/app/Main.java
+++ buchalka-master/Collections/src/com/app/Main.java
@@ -10,58 +10,44 @@ public class Main {
         Theatre theatre = new Theatre("Olympian", 8, 12);
 //        theatre.getSeats();
 
-//        if (theatre.reserveSeat("H11")) {
-//            System.out.println("Please pay, seat has been reserved");
-//        } else {
-//            System.out.println("Sorry, seat is taken");
-//        }
-//
-//        if (theatre.reserveSeat("H11")) {
-//            System.out.println("Please pay, seat has been reserved");
-//        } else {
-//            System.out.println("Sorry, seat is taken");
-//        }
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay, seat has been reserved");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
 
-        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats); // makes a shallow copy. different lists, but refers to same seat objects.
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay, seat has been reserved");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
 
-        //Collections.reverse(seatCopy);
-        Collections.shuffle(seatCopy);
+        if (theatre.reserveSeat("B13")) {
+            System.out.println("Please pay, seat has been reserved");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
 
+        // Collections.reverse is using the Comparable compareTo method for sorting
+        List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeats);
+        printList(reverseSeats);
 
-        System.out.println("Printing seat copy");
-        printList(seatCopy);
+        // Collections is using the Comparator object to sort
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        printList(priceSeats);
 
-        System.out.println("Printing theatre.seats");
-        printList(theatre.seats);
-
-        System.out.println("min seat: " + Collections.min(seatCopy).getSeatNumber());
-        System.out.println("max seat: " + Collections.max(seatCopy).getSeatNumber());
-
-        sortList(seatCopy);
-        System.out.println("Printing sorted seatCopy");
-        printList(seatCopy);
-
-        // Collections.copy does not work as expected...need particular conditions for it to work
-        // List<Theatre.Seat> newList = new ArrayList<>(theatre.seats.size());
-        // Collections.copy(newList, theatre.seats);
     }
 
     public static void printList(List<Theatre.Seat> list) {
         for (Theatre.Seat seat: list) {
-            System.out.println(" " + seat.getSeatNumber());
+            System.out.print(" " + seat.getSeatNumber() + " " + seat.getPrice() + ", ");
         }
         System.out.println();
         System.out.println("==================");
     }
 
-    // custom sort with Collections.swap
-    public static void sortList(List<? extends Theatre.Seat> list) {
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i).compareTo(list.get(j)) > 0) {
-                    Collections.swap(list, i, j);
-                }
-            }
-        }
-    }
 }
