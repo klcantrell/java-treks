b688f0fb9bf3612a38a4639c26b25df090af3e93
Kalalau L. Cantrell <klcantrell@sep.com>
2019-01-28 17:02:47 -0500

    buchalka: sets wip

---
+++ buchalka-master/SetsAndHashSet/src/com/kalcantrell/HeavenlyBody.java
@@ -0,0 +1,32 @@
+package com.kalcantrell;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satelllites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satelllites = new HashSet<HeavenlyBody>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satelllites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatelllites() {
+        return new HashSet<>(satelllites);
+    }
+}
