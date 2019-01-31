1085630cee84a0fbd6109a39dd011846befee25c
klcantrell <cantrellkalalau@gmail.com>
2019-01-30 19:43:06 -0500

    buchalka: sorted collections wip

--- buchalka-master/SortedCollections/src/com/kalcantrell/StockItem.java
+++ buchalka-master/SortedCollections/src/com/kalcantrell/StockItem.java
@@ -11,6 +11,12 @@ public class StockItem implements Comparable<StockItem> {
         this.quantityStock = 0;
     }
 
+    public StockItem(String name, double price, int quantityStock) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = quantityStock;
+    }
+
     public String getName() {
         return name;
     }
@@ -19,14 +25,57 @@ public class StockItem implements Comparable<StockItem> {
         return price;
     }
 
-    public int getQuantityStock() {
+    public int quantityInStock() {
         return quantityStock;
     }
 
     public void setPrice(double price) {
         if (price > 0d) {
-
+            this.price = price;
         }
         this.price = price;
+    }
+
+    public void adjustStock(int quantity) {
+        int newQuantity = this.quantityStock + quantity;
+        if (newQuantity >= 0) {
+            this.quantityStock = newQuantity;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("Entering StockItem.equals");
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        String objName = ((StockItem) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 31;
+    }
+
+    @Override
+    public int compareTo(StockItem o) {
+        System.out.println("Entering StockItem.compareTo");
+        if (this == o) {
+            return 0;
+        }
+        if (o != null) {
+            return this.name.compareTo(o.getName());
+        }
+        throw new NullPointerException("You need to pass in a StockItem");
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": " + this.price;
     }
 }
