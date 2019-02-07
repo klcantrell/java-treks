c278a83dab70f896984da88101c4fa4fc67bf17d
Kalalau L. Cantrell <klcantrell@sep.com>
2019-02-07 10:30:25 -0500

    buchalka: concurrency challenges 3, 4, 5

--- buchalka-master/ConcurrencyChallenges/src/com/app/Challenges3And4And5.java
+++ buchalka-master/ConcurrencyChallenges/src/com/app/Challenges3And4And5.java
@@ -1,5 +1,9 @@
 package com.app;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Challenges3And4And5 {
     public static void main(String[] args) {
         BankAccount3And4And5 bankAccount = new BankAccount3And4And5("12345-678", 1000.00);
@@ -27,28 +31,52 @@ public class Challenges3And4And5 {
 class BankAccount3And4And5 {
     private double balance;
     private String accountNumber;
+    private Lock lock;
 
     public BankAccount3And4And5(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
-    public synchronized void deposit(double amount) {
-        balance += amount;
-        System.out.println("Deposited " + amount + ". Balance is " + balance);
+    public void deposit(double amount) {
+        try {
+            if (lock.tryLock(100, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance += amount;
+                    System.out.println("Deposited " + amount + ". Balance is " + balance);
+                } finally {
+                    this.lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Could not get the lock");
+        }
     }
 
-    public synchronized void withdraw(double amount) {
-        balance -= amount;
-        System.out.println("Withdrew " + amount + ". Balance is " + balance);
+    public void withdraw(double amount) {
+        try {
+            if (lock.tryLock(100, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                    System.out.println("Withdrew " + amount + ". Balance is " + balance);
+                } finally {
+                    this.lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Something went wrong");
+        }
     }
 
-    // no need to synchronize... just reading.  this would introduce unnecessary performance issues.{
     public String getAccountNumber() {
         return accountNumber;
     }
 
-    // no need to synchronize... just reading.  this would introduce unnecessary performance issues.{
     public void printAccountNumber() {
         System.out.println("Account number is " + this.accountNumber);
     }
