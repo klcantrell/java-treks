1d35b7b501cd3fff998bde31396d234a975da528
klcantrell <cantrellkalalau@gmail.com>
2019-02-09 20:50:09 -0500

    buchalka: streams - terminal vs intermediate operations

--- buchalka-master/Streams/src/com/app/Main.java
+++ buchalka-master/Streams/src/com/app/Main.java
@@ -2,6 +2,7 @@ package com.app;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -22,10 +23,23 @@ public class Main {
 //        gNumbers.sort((s1, s2) -> s1.compareTo(s2));
 //        gNumbers.forEach(number -> System.out.println(number));
 
+        // with streams
         someBingoNumbers
                 .stream()
                 .map(String::toUpperCase)
                 .filter(s -> s.startsWith("G"))
-                .forEach(System.out::println);
+                .sorted()
+                .forEach(System.out::println); // terminal operation
+
+        System.out.println("creating our own streams");
+        Stream<String> iAndOStream = Stream.of("I26", "I17", "I29", "O71");
+        Stream<String> iAndNStream = Stream.of("N40", "N36", "I26", "I17", "I29", "O71");
+        Stream<String> concatStream = Stream.concat(iAndOStream, iAndNStream);
+        System.out.println(
+            concatStream
+                .distinct()
+                .peek(System.out::println)
+                .count()
+        );
     }
 }
