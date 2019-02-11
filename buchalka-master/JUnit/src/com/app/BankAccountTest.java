680f061faf2b737684f797ffeb9a4b814967436e
Kalalau L. Cantrell <klcantrell@sep.com>
2019-02-11 16:45:36 -0500

    buchalka: JUnit - asserts

--- buchalka-master/JUnit/src/com/app/BankAccountTest.java
+++ buchalka-master/JUnit/src/com/app/BankAccountTest.java
@@ -1,44 +1,72 @@
 package com.app;
 
+import com.app.BankAccount.AccountType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
-import com.app.BankAccount.AccountType;
 
 class BankAccountTest {
+    private BankAccount account;
+    private static int count;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Executes before all test cases. Count = " + count++);
+    }
+
+    @BeforeEach
+    public void setup() {
+        System.out.println("Running a test");
+        account = new BankAccount("Kal", "Cantrell", 1000.00, AccountType.CHECKING);
+    }
 
     @Test
     void deposit() {
-        BankAccount account = new BankAccount("Kal", "Cantrell", 1000.00, AccountType.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200, balance, 0);
     }
 
     @Test
-    void withdraw() {
-        fail("This test needs to be implemented");
+    void withdraw_branch() {
+        double balance = account.withdraw(600.00, true);
+        assertEquals(400.00, balance, 0);
+    }
+
+    @Test
+    void withdraw_notBranch() {
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(600.00, false));
     }
 
     @Test
     void getBalance_deposit() {
-        BankAccount account = new BankAccount("Kal", "Cantrell", 1000.00, AccountType.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200, account.getBalance(), 0);
     }
 
     @Test
     void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Kal", "Cantrell", 1000.00, AccountType.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(800, account.getBalance(), 0);
     }
 
     @Test
     void isChecking() {
-        BankAccount account = new BankAccount("Kal", "Cantrell", 1000.00, AccountType.SAVING);
         assertTrue(account.isChecking(), "The account is not a checking account");
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("This executes after each test case.  Count = " + count++);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Executes after all test cases. Count = " + count);
     }
 }
