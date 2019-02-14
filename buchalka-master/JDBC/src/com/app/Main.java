7b4c1fcc5b546b169ec6a38c47fb2fdd274acf70
Kalalau L. Cantrell <klcantrell@sep.com>
2019-02-14 12:48:14 -0500

    buchalka: jdbc - insert, update, delete wip

--- buchalka-master/JDBC/src/com/app/Main.java
+++ buchalka-master/JDBC/src/com/app/Main.java
@@ -8,10 +8,28 @@ import java.sql.Statement;
 public class Main {
 
     public static void main(String[] args) {
-	    try {
+        try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:databases\\testjava.db");
+            //conn.setAutoCommit(false);
+
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts" +
+                    "(name TEXT, phone INTEGER, email TEXT)");
+
+//            statement.execute("INSERT INTO contacts(name, phone, email) " +
+//                    "VALUES('Joe', 123123, 'joe@email.com')");
+//
+//            statement.execute("INSERT INTO contacts(name, phone, email) " +
+//                    "VALUES('Jane', 098098, 'jane@email.com')");
+//
+//            statement.execute("INSERT INTO contacts(name, phone, email) " +
+//                    "VALUES('Dudley', 8484, 'duds@email.com')");
+
+//            statement.execute("UPDATE contacts SET phone='01298' WHERE name='Jane'");
+
+            statement.execute("DELETE FROM contacts WHERE name='Joe'");
+
             statement.close();
             conn.close();
         } catch (SQLException e) {
