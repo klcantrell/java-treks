5e95f65247c6437ac15becd9530a30909c248d2e
klcantrell <cantrellkalalau@gmail.com>
2019-02-14 19:46:43 -0500

    buchalka: jdbc - executeQuery

--- buchalka-master/JDBC/src/com/app/Main.java
+++ buchalka-master/JDBC/src/com/app/Main.java
@@ -2,14 +2,22 @@ package com.app;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+    public static final String DB_NAME = "testjava.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:databases" + DB_NAME;
+
+    public static final String TABLE_CONTACTS = "contacts";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EMAIL = "email";
 
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:databases\\testjava.db");
+            Connection conn = DriverManager.getConnection(CONNECTION_STRING);
             //conn.setAutoCommit(false);
 
             Statement statement = conn.createStatement();
@@ -28,7 +36,23 @@ public class Main {
 
 //            statement.execute("UPDATE contacts SET phone='01298' WHERE name='Jane'");
 
-            statement.execute("DELETE FROM contacts WHERE name='Joe'");
+//            statement.execute("DELETE FROM contacts WHERE name='Joe'");
+
+            // querying with .execute
+//            statement.execute("SELECT * FROM contacts");
+//            ResultSet results = statement.getResultSet();
+
+            // querying with .executeQuery
+            ResultSet results = statement.executeQuery("SELECT * FROM contacts");
+            while (results.next()) {
+                System.out.println(
+                        results.getString(COLUMN_NAME) + " " +
+                        results.getString(COLUMN_PHONE) + " " +
+                        results.getString(COLUMN_EMAIL)
+                );
+            }
+
+            results.close();
 
             statement.close();
             conn.close();
