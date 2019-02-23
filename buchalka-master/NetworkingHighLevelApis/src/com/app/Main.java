5c60d62f2516cff27bd568f9115eaefe932d5f01
klcantrell <cantrellkalalau@gmail.com>
2019-02-23 16:30:03 -0500

    buchalka: networking - the HttpURLConnection object

--- buchalka-master/NetworkingHighLevelApis/src/com/app/Main.java
+++ buchalka-master/NetworkingHighLevelApis/src/com/app/Main.java
@@ -3,60 +3,61 @@ package com.app;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            // not an absolute path, evidently
-            //URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os-android#samsung");
-
-            // http = known scheme --> absolute path
-            //URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os-android#samsung");
-
-            // base URI - couple this with other URI components to create a URL
-//            URI baseUri = new URI("http://username:password@mynewserver.com:5000");
-
-            // relative URI - won't turn into URL by itself (URL needs an absolute URI)
-//            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
-//            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
-//            URI uri3 = new URI("/stores/locations?zip=samsung");
-//
-//            URI resolvedUri1 = baseUri.resolve(uri1);
-//            URI resolvedUri2 = baseUri.resolve(uri2);
-//            URI resolvedUri3 = baseUri.resolve(uri3);
-
-//            URL url1 = resolvedUri1.toURL();
-//            System.out.println("URL1 = " + url1);
-//            URL url2 = resolvedUri2.toURL();
-//            System.out.println("URL2 = " + url2);
-//            URL url3 = resolvedUri3.toURL();
-//            System.out.println("URL3 = " + url3);
-//
-//            URI relativizedUri = baseUri.relativize(resolvedUri1);
-//            System.out.println("Relative URI = " + relativizedUri);
-//
             URL url = new URL("http://example.org");
-//            URI uri = url.toURI();
-//
-//            System.out.println("Scheme = " + uri.getScheme());
-//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-//            System.out.println("Authority = " + uri.getAuthority());
-//            System.out.println("User info = " + uri.getUserInfo());
-//            System.out.println("Host = " + uri.getHost());
-//            System.out.println("Port = " + uri.getPort());
-//            System.out.println("Path = " + uri.getPath());
-//            System.out.println("Query = " + uri.getQuery());
-//            System.out.println("Fragment = " + uri.getFragment());
 
-            // use an inputstream to read contents from a URL resource
+//            URLConnection urlConnection = url.openConnection();
+//            urlConnection.setDoOutput(true);
+//            urlConnection.connect();
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "MyUserAgent");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code = " + responseCode);
+            connection.setReadTimeout(5000);
+
+            if (responseCode != 200) {
+                System.out.println("Error reading web page");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream())
+            );
+
+            String line;
+
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
 //            BufferedReader inputStream = new BufferedReader(
-//                    new InputStreamReader(url.openStream())
+//                    new InputStreamReader(urlConnection.getInputStream())
 //            );
+
+//            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
 //
+//            headerFields.entrySet().forEach(headerField -> {
+//                String key = headerField.getKey();
+//                List<String> value = headerField.getValue();
+//                System.out.println("--------key = " + key);
+//                value.forEach(item -> {
+//                    System.out.println("value = " + item);
+//                });
+//            });
+
 //            String line = "";
 //
 //            while (line != null) {
@@ -66,27 +67,6 @@ public class Main {
 //
 //            inputStream.close();
 
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
-
-            BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream())
-            );
-
-            String line = "";
-
-            while (line != null) {
-                line = inputStream.readLine();
-                System.out.println(line != null ? line : "");
-            }
-
-            inputStream.close();
-
-//        } catch (URISyntaxException e) {
-//            System.out.println("URI bad Syntax: " + e.getIndex());
-//        } catch (MalformedURLException e) {
-//            System.out.println("URL Malformed: " + e.getMessage());
         } catch (MalformedURLException e) {
                 System.out.println("Malformed URL: " + e.getMessage());
         } catch (IOException e) {
