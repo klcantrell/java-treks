e58e63ea312e44d79fe9b54f439c565d701d87c4
klcantrell <cantrellkalalau@gmail.com>
2019-02-23 22:14:34 -0500

    28mins-springboot: intro to spring - using qualifier annotations

--- 28minutes-springboot/spring-basics/src/main/java/com/app/springbasics/BubbleSortAlgorithm.java
+++ 28minutes-springboot/spring-basics/src/main/java/com/app/springbasics/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.app.springbasics;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("bubbleSort")
 public class BubbleSortAlgorithm implements SortAlgorithm {
     public int[] sort (int[] numbers) {
         // logic for bubble sort
--- 28minutes-springboot/spring-basics/src/main/java/com/app/springbasics/QuickSortAlgorithm.java
+++ 28minutes-springboot/spring-basics/src/main/java/com/app/springbasics/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.app.springbasics;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("quickSort")
 public class QuickSortAlgorithm implements SortAlgorithm {
     public int[] sort(int[] numbers) {
         // logic for quick sort
