693ce8d2b710e4597e8aaa9bd321f376cd38d1fb
klcantrell <cantrellkalalau@gmail.com>
2019-02-26 21:46:07 -0500

    28min-sprinboot: simple web app - controller/welcome jsp

--- 28minutes-springboot/spring-boot-first-web-app/src/main/java/com/kalcantrell/springbootfirstwebapp/controller/LoginController.java
+++ 28minutes-springboot/spring-boot-first-web-app/src/main/java/com/kalcantrell/springbootfirstwebapp/controller/LoginController.java
@@ -5,13 +5,19 @@ import com.kalcantrell.springbootfirstwebapp.Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginMessage(Parameters params, ModelMap model) {
         model.put("name", params.getName());
         return "login";
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String showWelcomePage(Parameters params, ModelMap model) {
+        model.put("name", params.getName());
+        return "welcome";
     }
 }
--- 28minutes-springboot/spring-boot-first-web-app/src/main/webapp/WEB-INF/jsp/login.jsp
+++ 28minutes-springboot/spring-boot-first-web-app/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -2,6 +2,13 @@
     <head>
         <title>First Web Application</title>
     </head>
+    <form method="POST">
+        <label for="name">Name: </label>
+        <input type="text" name="name" id="name">
+        <label for="password">Password: </label>
+        <input type="password" name="password" id="password">
+        <button>Submit Query</button>
+    </form>
     <body>
         My First JSP! ${name}
     </body>
