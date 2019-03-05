f00ee00d54832da8062cab4cb8d97702475840eb
klcantrell <cantrellkalalau@gmail.com>
2019-03-04 20:28:12 -0500

    28minutes-springboot: error handling with spring

---
+++ 28minutes-springboot/spring-boot-first-web-app/src/main/java/com/kalcantrell/springbootfirstwebapp/Controller/ErrorController.java
@@ -0,0 +1,22 @@
+package com.kalcantrell.springbootfirstwebapp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller("error")
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(HttpServletRequest request, HttpServletResponse response, Exception ex) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("exception", ex.getStackTrace());
+        mv.addObject("error", request.getRequestURL());
+        mv.setViewName("error");
+        return mv;
+    }
+
+}
