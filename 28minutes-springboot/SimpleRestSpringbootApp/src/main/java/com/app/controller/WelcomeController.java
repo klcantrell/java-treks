8b40f60b095a4126cdbb7a88e3c60657dbb7fbed
klcantrell <cantrellkalalau@gmail.com>
2019-03-14 21:18:43 -0400

    28minutes-springboot: monitoring and configuration

--- 28minutes-springboot/SimpleRestSpringbootApp/src/main/java/com/app/controller/WelcomeController.java
+++ 28minutes-springboot/SimpleRestSpringbootApp/src/main/java/com/app/controller/WelcomeController.java
@@ -1,19 +1,36 @@
 package com.app.controller;
 
+import com.app.configuration.BasicConfiguration;
 import com.app.service.WelcomeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WelcomeController {
 
-    //Auto wiring
-    @Autowired
     private WelcomeService service;
+    private final BasicConfiguration basicConfiguration;
+
+    public WelcomeController(WelcomeService welcomeService, BasicConfiguration basicConfiguration) {
+        service = welcomeService;
+        this.basicConfiguration = basicConfiguration;
+    }
 
     @RequestMapping("/welcome")
     public String welcome() {
         return service.retrieveWelcomeMessage();
+    }
+
+    @RequestMapping("/dynamic-configuration")
+    public Map dynamicConfiguration() {
+        Map map = new HashMap();
+        map.put("message", basicConfiguration.getMessage());
+        map.put("number", basicConfiguration.getNumber());
+        map.put("value", basicConfiguration.value());
+
+        return map;
     }
 }
--- 28minutes-springboot/SimpleRestSpringbootApp/src/main/java/com/app/service/WelcomeService.java
+++ 28minutes-springboot/SimpleRestSpringbootApp/src/main/java/com/app/service/WelcomeService.java
@@ -1,12 +1,15 @@
 package com.app.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WelcomeService {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     public String retrieveWelcomeMessage() {
-        //Complex Method
-        return "Good Morning updated";
+        return welcomeMessage;
     }
 }
